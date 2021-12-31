9563b02d6fb68837807e5b006e0ad0c4fcef8a0c
Ezequiel Binker <ezebinker@gmail.com>
2021-12-31 16:26:35 -0300

    fix: years calculated, dev skills

---
+++ scripts/age.js
@@ -0,0 +1,23 @@
+(function () {
+    "use strict";
+
+    window.addEventListener('load', () => {
+        document.getElementById("yearsEducator").innerHTML = getAge("2014/06/23")
+        document.getElementById("age").innerHTML= getAge("1994/12/04")
+    });
+
+    /**
+     * Function gets called when page is loaded.
+     */
+    function getAge(dateParam) {
+        const today = new Date();
+        const birthDate = new Date(dateParam);
+        const m = today.getMonth() - birthDate.getMonth();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age
+    }
+
+})();
