abda7120fdb2dc6efb3fe2777599728cadc44513
Ezequiel Binker <ezebinker@gmail.com>
2026-06-28 21:36:42 -0300

    Remove AOS initialization and related load event listener to streamline script execution

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,6 @@
 (function() {
   "use strict";
 
-  window.addEventListener('load', () => {
-    on_page_load()
-  });
-
-  /**
-   * Function gets called when page is loaded.
-   */
-  function on_page_load() {
-    // Initialize On-scroll Animations
-    AOS.init({
-      anchorPlacement: 'top-left',
-      duration: 400,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-      disable: 'mobile'
-    });
-  }
-
   /**
    * Navbar effects and scrolltop buttons upon scrolling
    */
